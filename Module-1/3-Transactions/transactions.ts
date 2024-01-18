f8d1c39b24f1ccfd6812af3814b34b1b607c47b7
Henry Tirla <henrytirla@gmail.com>
2024-01-18 16:06:25 +0100

    Sending Sol

--- Module-1/3-Transactions/transactions.ts
+++ Module-1/3-Transactions/transactions.ts
@@ -1,17 +1,24 @@
-import {Connection, clusterApiUrl, PublicKey,LAMPORTS_PER_SOL,Transaction,SystemProgram} from "@solana/web3.js";
+import {Connection, clusterApiUrl, PublicKey,LAMPORTS_PER_SOL,Transaction,SystemProgram,sendAndConfirmTransaction} from "@solana/web3.js";
+
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+
 
 const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(process.env.KEY_PAIR)
+const senderKeypair = getKeypairFromEnvironment("KEY_PAIR");
 
 
 const transaction = new Transaction()
 
-const sender = new PublicKey("CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN");
-const receiver = new PublicKey("CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN");
+const sender = new PublicKey("4dvZZmw5PhoQRSDz3KgzfZkioxa7PZjJuRvguwzSwxK7");
+const receiver = new PublicKey("En5z1QWHbUtWdd54mdK1QVEBSpvDwejZDkroFAbccXbD");
 
 const amount = 1; // Amount of sol to send
 
 const sendSolInstruction = SystemProgram.transfer({
-    fromPubkey:sender,
+    fromPubkey:senderKeypair.publicKey,
     toPubkey:receiver,
     lamports:LAMPORTS_PER_SOL * amount
 
@@ -19,3 +26,13 @@ const sendSolInstruction = SystemProgram.transfer({
 });
 
 transaction.add(sendSolInstruction);
+
+// @ts-ignore
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+]);
+
+console.log(
+    `💸 Finished! Sent ${amount} to the address ${receiver}. `
+);
+console.log(`Transaction signature is ${signature}!`);
